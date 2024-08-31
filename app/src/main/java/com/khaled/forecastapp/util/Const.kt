079c33a8d436d03cf3cf24c96f3712e3aa87784c
Khaled Elramam <Elramam.c@channels.com.sa>
2024-09-01 00:33:26 +0300

    Add City Selector - Add CitySelector drop down - Add Static city list - Add moshi and retrofit dependencies

---
+++ app/src/main/java/com/khaled/forecastapp/util/Const.kt
@@ -0,0 +1,31 @@
+package com.khaled.forecastapp.util
+
+import com.khaled.forecastapp.home.domain.City
+
+val cityList = listOf(
+    City(
+        cityNameAr = "القاهرة", cityNameEn = "Cairo", id = 1, lat = 30.0444, lon = 31.2357
+    ), City(
+        cityNameAr = "دبي", cityNameEn = "Dubai", id = 2, lat = 25.2048, lon = 55.2708
+    ), City(
+        cityNameAr = "باريس", cityNameEn = "Paris", id = 3, lat = 48.8566, lon = 2.3522
+    ), City(
+        cityNameAr = "لندن", cityNameEn = "London", id = 4, lat = 51.5074, lon = -0.1278
+    ), City(
+        cityNameAr = "نيويورك", cityNameEn = "New York", id = 5, lat = 40.7128, lon = -74.0060
+    ), City(
+        cityNameAr = "طوكيو", cityNameEn = "Tokyo", id = 6, lat = 35.6762, lon = 139.6503
+    ), City(
+        cityNameAr = "سنغافورة", cityNameEn = "Singapore", id = 7, lat = 1.3521, lon = 103.8198
+    ), City(
+        cityNameAr = "ريو دي جانيرو",
+        cityNameEn = "Rio de Janeiro",
+        id = 8,
+        lat = -22.9068,
+        lon = -43.1729
+    ), City(
+        cityNameAr = "سيدني", cityNameEn = "Sydney", id = 9, lat = -33.8688, lon = 151.2093
+    ), City(
+        cityNameAr = "موسكو", cityNameEn = "Moscow", id = 10, lat = 55.7558, lon = 37.6173
+    )
+)
