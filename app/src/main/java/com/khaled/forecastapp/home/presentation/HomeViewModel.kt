821271a00a60e1af40b68a186b58b4f2092ca996
Khaled Elramam <Elramam.c@channels.com.sa>
2024-09-01 19:26:07 +0300

    prepare presentation layer

--- app/src/main/java/com/khaled/forecastapp/home/presentation/HomeViewModel.kt
+++ app/src/main/java/com/khaled/forecastapp/home/presentation/HomeViewModel.kt
@@ -3,19 +3,20 @@ package com.khaled.forecastapp.home.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.khaled.forecastapp.home.domain.City
-import com.khaled.forecastapp.home.domain.api.WeatherResponseDTO
+import com.khaled.forecastapp.home.domain.api.WeatherUi
 import com.khaled.forecastapp.home.domain.repo.HomeRepo
 import com.khaled.forecastapp.util.Resource
 import com.khaled.forecastapp.util.cityList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repo: HomeRepo) : ViewModel() {
-    private var _weatherFlow = MutableStateFlow<Resource<WeatherResponseDTO>>(Resource.Idle)
-    val weatherFlow = _weatherFlow
+    private var _weatherFlow = MutableStateFlow<Resource<List<WeatherUi>>>(Resource.Idle)
+    val weatherFlow: StateFlow<Resource<List<WeatherUi>>> = _weatherFlow
 
     fun getWeatherByCity(city: City) {
         _weatherFlow.value = Resource.Loading
